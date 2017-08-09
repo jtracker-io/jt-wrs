6a3f98caf3d6293481e7c2a52913fc3f7632f90f
Junjun Zhang <junjun.ca@gmail.com>
2017-08-09 14:26:22 -0400

    refactoring to reflect data model changes

--- jt_wrs/wrs.py
+++ jt_wrs/wrs.py
@@ -29,71 +29,89 @@ def get_workflows(account_name, workflow_name=None, workflow_version=None):
     account_id = _get_account_id_by_name(account_name)
 
     if account_id:
-        workflows = {}
+        workflows = []
 
-        key_prefix = '/'.join([WRS_ETCD_ROOT,
-                               'account._id:%s/' % account_id])
+        # find the workflows' name and id first
+        workflow_name_id_prefix = '/'.join([WRS_ETCD_ROOT,
+                                            'account._id:%s' % account_id,
+                                            'workflow/'])
 
-        # ideally we don't read values yet, but python-etcd does not have this option
-        r = etcd_client.get_prefix(key_prefix=key_prefix +
-                                              ('' if not workflow_name else 'workflow._name:%s/' % workflow_name),
-                                   sort_target='KEY')
+        if workflow_name:
+            key_search_prefix = '%s_name:%s/_id' % (workflow_name_id_prefix, workflow_name)
+        else:
+            key_search_prefix = '%s_name' % workflow_name_id_prefix
+
+        r = etcd_client.get_prefix(key_prefix=key_search_prefix, sort_target='KEY')
 
         for value, meta in r:
-            k = meta.key.decode('utf-8').replace(key_prefix, '', 1)
+            k = meta.key.decode('utf-8').replace(workflow_name_id_prefix, '', 1)
             try:
                 v = value.decode("utf-8")
             except:
                 v = None  # assume binary value, deal with it later
-            print(k, v)
-            parts = k.split('/')
 
-            new_key, _name = parts[0].split(':', 1)
-            if new_key == 'workflow._name' and not workflows.get(_name):
-                workflows[_name] = {}
+            print("k:%s, v:%s" % (k, v))
 
-            if ':' in parts[-1]:
-                new_key, new_value = parts[-1].split(':', 1)
-            else:
-                new_key, new_value = parts[-1], v
+            workflow_name = k.strip('_name:').rstrip('/_id')
+            workflow_id = v
 
-            if isinstance(new_value, str):
-                new_value = new_value.replace('+', '/')
+            workflow = {
+                "_id": workflow_id,
+                "_name": workflow_name
+            }
 
-            if new_key.startswith('is_'):
-                new_value = True if new_value and new_value != '0' else False
+            # ideally we don't read values yet, but python-etcd does not have this option
+            workflow_prefix = '/'.join([WRS_ETCD_ROOT, 'workflow', '_id:%s/' % workflow_id])
 
-            if len(parts) == 2:
-                if '@' not in new_key:
-                    workflows[_name][new_key] = new_value
+            r2 = etcd_client.get_prefix(key_prefix=workflow_prefix, sort_target='KEY')
+
+            for value2, meta2 in r2:
+                k = meta2.key.decode('utf-8').replace(workflow_prefix, '', 1)
+                try:
+                    v = value2.decode("utf-8")
+                except:
+                    v = None  # assume binary value, deal with it later
+                print("k:%s, v:%s" % (k, v))
+                parts = k.split('/')
+
+                if ':' in parts[-1]:
+                    new_key, new_value = parts[-1].split(':', 1)
                 else:
-                    sub_key, sub_type = new_key.split('@', 1)
-                    if not sub_type in workflows[_name]: workflows[_name][sub_type] = []
-                    workflows[_name][sub_type].append({sub_key: new_value})
+                    new_key, new_value = parts[-1], v
 
-            elif len(parts) == 3:
-                ver_tag, ver = parts[1].split(':', 1)
-                if workflow_version and not workflow_version == ver:
-                    continue
+                if isinstance(new_value, str):
+                    new_value = new_value.replace('+', '/')
 
-                ver = 'ver:%s' % ver
-                if ver_tag == 'ver' and not workflows[_name].get(ver):
-                    workflows[_name][ver] = {}
+                if new_key.startswith('is_'):
+                    new_value = True if new_value and new_value != '0' else False
 
-                if '@' not in new_key:
-                    workflows[_name][ver][new_key] = new_value
-                else:
-                    sub_key, sub_type = new_key.split('@', 1)
-                    if not sub_type in workflows[_name][ver]: workflows[_name][ver][sub_type] = []
-                    workflows[_name][ver][sub_type].append({sub_key: new_value})
+                if len(parts) == 1:
+                    if '@' not in new_key:
+                        workflow[new_key] = new_value
+                    else:
+                        sub_key, sub_type = new_key.split('@', 1)
+                        if not sub_type in workflow: workflow[sub_type] = []
+                        workflow[sub_type].append({sub_key: new_value})
 
-        ret = []
-        for w in workflows:
-            workflow = workflows[w]
-            workflow.update({'workflow_name': w})
-            ret.append(workflow)
+                elif len(parts) == 2:
+                    ver_tag, ver = parts[0].split(':', 1)
+                    if workflow_version and not workflow_version == ver:
+                        continue
 
-        return ret
+                    ver = 'ver:%s' % ver
+                    if ver_tag == 'ver' and not workflow.get(ver):
+                        workflow[ver] = {}
+
+                    if '@' not in new_key:
+                        workflow[ver][new_key] = new_value
+                    else:
+                        sub_key, sub_type = new_key.split('@', 1)
+                        if sub_type not in workflow[ver]: workflow[ver][sub_type] = []
+                        workflow[ver][sub_type].append({sub_key: new_value})
+
+            workflows.append(workflow)
+
+        return workflows
     else:
         raise AccountNameNotFound(Exception("Specific account name not found: %s" % account_name))
 
